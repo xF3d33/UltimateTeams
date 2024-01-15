48af494ef15f8b3366c21933e03103e99848cbda
Federico Russo <russo.federico451@gmail.com>
2024-01-15 14:50:42 +0100

    Update HuskHomesAPIHook.java

--- src/main/java/dev/xf3d3/ultimateteams/hooks/HuskHomesAPIHook.java
+++ src/main/java/dev/xf3d3/ultimateteams/hooks/HuskHomesAPIHook.java
@@ -4,69 +4,61 @@ import dev.xf3d3.ultimateteams.UltimateTeams;
 import net.william278.huskhomes.api.HuskHomesAPI;
 import net.william278.huskhomes.position.Position;
 import net.william278.huskhomes.position.World;
+import net.william278.huskhomes.teleport.TeleportBuilder;
 import net.william278.huskhomes.teleport.TeleportationException;
 import net.william278.huskhomes.user.OnlineUser;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.logging.Level;
 
 public class HuskHomesAPIHook {
 
-    private final HuskHomesAPI huskHomesAPI;
+    @Nullable
+    private final HuskHomesAPI api;
+
     private final UltimateTeams plugin;
 
     public HuskHomesAPIHook(@NotNull UltimateTeams plugin) {
-        this.huskHomesAPI = HuskHomesAPI.getInstance();
+        this.api = HuskHomesAPI.getInstance();
         this.plugin = plugin;
         sendMessages();
     }
 
-    public void teleportPlayer(@NotNull Player player, @NotNull Location location) {
-        OnlineUser onlineUser = huskHomesAPI.adaptUser(player);
 
-        Position position = Position.at(
-                location.getX(),
-                location.getY(),
-                location.getZ(),
-                World.from(Objects.requireNonNull(location.getWorld()).getName(), UUID.randomUUID()),
-                ""
-        );
-
+    public void teleport(@NotNull Player player, @NotNull dev.xf3d3.ultimateteams.team.Position position, @NotNull String server,
+                         boolean instant) {
         try {
-            huskHomesAPI.teleportBuilder()
-                    .teleporter(onlineUser)
-                    .target(position)
-                    .toTimedTeleport()
-                    .execute();
-        } catch (TeleportationException e) {
-            plugin.log(Level.SEVERE, "error while trying to teleport player with HuskHomes" , e);
+            final HuskHomesAPI api = getHuskHomes()
+                    .orElseThrow(() -> new IllegalStateException("HuskHomes API not present"));
+            final TeleportBuilder builder = api
+                    .teleportBuilder(api.adaptUser(player))
+                    .target(net.william278.huskhomes.position.Position.at(
+                            position.getX(),
+                            position.getY(),
+                            position.getZ(),
+                            position.getYaw(),
+                            position.getPitch(),
+                            net.william278.huskhomes.position.World.from(position.getWorld().getName(), position.getWorld().getUID()),
+                            server
+                    ));
+            if (instant) {
+                builder.toTeleport().execute();
+            } else {
+                builder.toTimedTeleport().execute();
+            }
+        } catch (IllegalStateException e) {
+            // TODO: send message
         }
     }
 
-    public void teleportPlayer(@NotNull Player player, @NotNull String server, @NotNull Location location) {
-        OnlineUser onlineUser = huskHomesAPI.adaptUser(player);
-
-        Position position = Position.at(
-                location.getX(),
-                location.getY(),
-                location.getZ(),
-                World.from(Objects.requireNonNull(location.getWorld()).getName(), UUID.randomUUID()),
-                server
-        );
-
-        try {
-            huskHomesAPI.teleportBuilder()
-                    .teleporter(onlineUser)
-                    .target(position)
-                    .toTimedTeleport()
-                    .execute();
-        } catch (TeleportationException e) {
-            plugin.log(Level.SEVERE, "error while trying to teleport player with HuskHomes on another server" , e);
-        }
+    private Optional<HuskHomesAPI> getHuskHomes() {
+        return Optional.ofNullable(api);
     }
 
     private void sendMessages() {
